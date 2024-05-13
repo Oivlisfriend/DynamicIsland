330ca60d5361eb74f832460f287febf16f904fbf
Oivlisfriend <robertomarcelogongo@gnail.com>
2024-05-13 22:53:38 +0100

    Initial Commit

---
+++ TimeTrackingAttribute/TimeTrackingAttributeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTrackingAttributeApp.swift
+//  TimeTrackingAttribute
+//
+//  Created by Dumilde on 13/05/24.
+//
+
+import SwiftUI
+
+@main
+struct TimeTrackingAttributeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
